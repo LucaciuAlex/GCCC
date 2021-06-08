4afb32e60773213c2ebf2ee3d9f83ceb50a2de24
Alex Bodea <alexandrubodeag@gmail.com>
2021-06-08 19:35:59 +0300

    Refactored + fix

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const { BlobServiceClient } = require("@azure/storage-blob");
 
-const listButton = document.getElementById("listButton");
 const fileList = document.getElementById("file-list");
 const fileInput = document.getElementById("file-input");
 
 const accountName = "guestbookreviews";
 const sasString = "se=2021-06-10&sp=rwdlac&sv=2018-03-28&ss=b&srt=sco&sig=vds4LBqrkD0nCAOxLoT8bdtQcn0S1gt2hppBReI34xE%3D";
-const accessKey = "OpXrkY1291nZ7C289q2ZD/KyKQ9sNYW1bgkSzQcKlF84AMtCtGxQrvH1rslkcJSEK+LFvS9viXtnwRF3DbdmKw=="
 const containerName = "reviews";
-const AZURE_STORAGE_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=guestbookreviews;AccountKey=OpXrkY1291nZ7C289q2ZD/KyKQ9sNYW1bgkSzQcKlF84AMtCtGxQrvH1rslkcJSEK+LFvS9viXtnwRF3DbdmKw==;EndpointSuffix=core.windows.net'
 const imageUrlRoot = "https://guestbookreviews.blob.core.windows.net/reviews/";
 const blobServiceClient = new BlobServiceClient(`https://${accountName}.blob.core.windows.net/?${sasString}`);
 const containerClient = blobServiceClient.getContainerClient(containerName);
 let photo;
 
 const listFiles = async () => {
-
     try {
         let iter = containerClient.listBlobsFlat();
         let blobItem = await iter.next();
@@ -24,15 +20,8 @@ const listFiles = async () => {
             console.log(blobItem);
             const url = `${imageUrlRoot}${blobItem.value.name}`;
             fileList.innerHTML +=
-                // <option>${blobItem.value.name}</option>;
                 '<div class="mb-1 image-container"><a href="'+ url + '" target="_blank"><img src="' + url + '" alt="" class="image"></a><span>' + blobItem.value.name + '</span></div>';
             blobItem = await iter.next();
-        }
-        if (fileList.size > 0) {
-            console.log("Done");
-        } else {
-            console.log("Not");
-
         }
     } catch (error) {
         console.log(error);
@@ -54,11 +43,12 @@ const uploadFiles = async () => {
     }
 }
 
-function savePhoto(inp) {
-    photo = inp.files[0];
+function savePhoto() {
+    photo = fileInput.files[0];
 }
 
-// selectButton.addEventListener("click", () => fileInput.click());
-fileInput.addEventListener("change", uploadFiles);
+fileInput.addEventListener("change", savePhoto);
 
-listFiles().then(() => console.log('Done')).catch((ex) => console.log(ex.message));
+window.onload = (event) => {
+    listFiles().then();
+};
